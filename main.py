ebee61a2efccbf6ca1f3632de3e58deaf7d10343
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-03-19 11:13:09 +1100

    Added initial files

--- main.py
+++ main.py
@@ -2,4 +2,10 @@
 #Takes in text representing board state and option (Possible moves or path to victory)
 #Returns answer to option based on board state
 
-inputData = input()
+#read input (parse.py (parseInput()))
+
+#if line 9 is find moves (parse.py (parse.input))
+#calculate moves (findMoves.py (findMoves()))
+
+#Else
+#Find path (findPath.py (findPath()))
