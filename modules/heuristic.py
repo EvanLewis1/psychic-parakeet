f72d2dbc13a9a5532af1b96738815fb7b7b065f0
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-05-09 12:48:38 +1000

    Created new heuristic function that takes board state and colour and gives a number representing the advantage the given colour has

--- modules/heuristic.py
+++ modules/heuristic.py
@@ -24,3 +24,34 @@ def heuristic(oldBoard, newBoard):
     # Final value combines all elements
     value = takenPiecesVal + randVal + dangerVal
     return value
+
+###########################################################
+PIECEWORTH = 10
+PIECEINDANGERWORTH = 3
+
+MAXADVANTAGE = PIECEWORTH * 9 + PIECEINDANGERWORTH * 3
+MINADVANTAGE = -MAXADVANTAGE
+
+#One player has all 12 pieces, other has 3, all in danger
+
+#Returns chance of winning based on board state
+def EvanHeuristic(board, colour):
+
+    #Victory if opponent has two or less pieces
+    if len(board.colourPiecesInfo(board.oppcolour(colour))) < 3:
+        return MAXADVANTAGE + 1
+
+    #Get piece advantage/disadvantage
+    pieceAdvantage = len(board.colourPiecesInfo(board.oppcolour(colour))) - len(board.colourPiecesInfo(colour))
+
+    #Get piece inDanger advantage/disadvantage
+    opposingPiecesInDanger = len([piece for piece in board.colourPiecesInfo(board.oppcolour(colour)) if piece.inDanger()])
+    piecesInDanger = len([piece for piece in board.colourPiecesInfo(colour) if piece.inDanger()])
+
+    piecesInDangerAdvantage = opposingPiecesInDanger - piecesInDanger
+
+    #Calculate overall advantage
+    advantage = pieceAdvantage * PIECEWORTH + piecesInDangerAdvantage * PIECEINDANGERWORTH
+
+    return advantage
+
