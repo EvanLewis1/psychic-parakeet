9abb49d03703a168726260fe01b2ae9da3b230aa
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-05-09 13:07:47 +1000

    Implemented new heuristic

--- modules/heuristic.py
+++ modules/heuristic.py
@@ -1,29 +1,30 @@
 from modules.models.Rules import *
 from random import *
+from modules.models import Piece
 
 
 # Creates a value for a given board state
-def heuristic(oldBoard, newBoard):
-
-    # Removing a piece is given a very high value, losing a piece is given a very low value
-    #takenPiecesVal = ((len(oldBoard.colourPiecesInfo(BLACK)) - len(newBoard.colourPiecesInfo(BLACK))) -
-                     # (len(oldBoard.colourPiecesInfo(WHITE)) - len(newBoard.colourPiecesInfo(WHITE)))) * 10
-
-    takenPiecesVal = (len(oldBoard.colourPiecesInfo(BLACK)) - len(newBoard.colourPiecesInfo(BLACK))) * 10
-
-    # The number of adjacent pieces (pieces in danger)
-    dangerVal = 0
-
-    for piece in newBoard.colourPiecesInfo(BLACK):
-        if piece.inDanger(newBoard.currentState):
-            dangerVal += 3
-
-    # Random int to prevent repeating unhelpful moves
-    randVal = randint(1, 2)
-
-    # Final value combines all elements
-    value = takenPiecesVal + randVal + dangerVal
-    return value
+# def heuristic(oldBoard, newBoard):
+#
+#     # Removing a piece is given a very high value, losing a piece is given a very low value
+#     #takenPiecesVal = ((len(oldBoard.colourPiecesInfo(BLACK)) - len(newBoard.colourPiecesInfo(BLACK))) -
+#                      # (len(oldBoard.colourPiecesInfo(WHITE)) - len(newBoard.colourPiecesInfo(WHITE)))) * 10
+#
+#     takenPiecesVal = (len(oldBoard.colourPiecesInfo(BLACK)) - len(newBoard.colourPiecesInfo(BLACK))) * 10
+#
+#     # The number of adjacent pieces (pieces in danger)
+#     dangerVal = 0
+#
+#     for piece in newBoard.colourPiecesInfo(BLACK):
+#         if piece.inDanger(newBoard.currentState):
+#             dangerVal += 3
+#
+#     # Random int to prevent repeating unhelpful moves
+#     randVal = randint(1, 2)
+#
+#     # Final value combines all elements
+#     value = takenPiecesVal + randVal + dangerVal
+#     return value
 
 ###########################################################
 PIECEWORTH = 10
@@ -35,20 +36,21 @@ MINADVANTAGE = -MAXADVANTAGE
 #One player has all 12 pieces, other has 3, all in danger
 
 #Returns chance of winning based on board state
-def EvanHeuristic(board, colour):
+def heuristic(board, colour):
 
     #Victory if opponent has two or less pieces
-    if len(board.colourPiecesInfo(board.oppcolour(colour))) < 3:
+    if len(board.colourPiecesInfo(Piece.Piece.opposite(colour))) < 3:
         return MAXADVANTAGE + 1
 
     #Get piece advantage/disadvantage
-    pieceAdvantage = len(board.colourPiecesInfo(board.oppcolour(colour))) - len(board.colourPiecesInfo(colour))
+    pieceAdvantage = len(board.colourPiecesInfo(colour)) - len(board.colourPiecesInfo(Piece.Piece.opposite(colour)))
 
     #Get piece inDanger advantage/disadvantage
-    opposingPiecesInDanger = len([piece for piece in board.colourPiecesInfo(board.oppcolour(colour)) if piece.inDanger()])
-    piecesInDanger = len([piece for piece in board.colourPiecesInfo(colour) if piece.inDanger()])
-
-    piecesInDangerAdvantage = opposingPiecesInDanger - piecesInDanger
+    # opposingPiecesInDanger = len([piece for piece in board.colourPiecesInfo(Piece.Piece.opposite(colour)) if piece.inDanger()])
+    # piecesInDanger = len([piece for piece in board.colourPiecesInfo(colour) if piece.inDanger()])
+    #
+    # piecesInDangerAdvantage = opposingPiecesInDanger - piecesInDanger
+    piecesInDangerAdvantage = 0
 
     #Calculate overall advantage
     advantage = pieceAdvantage * PIECEWORTH + piecesInDangerAdvantage * PIECEINDANGERWORTH
